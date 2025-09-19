080db041db60163d989e57cd02107d00b1df3493
wizisergio <wizisergio@gmail.com>
2025-09-19 09:37:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import Botoncito from './components/Buttons/Botoncito';
 import Card from './components/Card/Card';
 import './App.css';
@@ -56,9 +55,3 @@ function App() {
 }
 
 export default App;
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
